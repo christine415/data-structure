b3bc6cc328acf2900d78d3e50297e1723e607225
christine415 <yichaochen415@gmail.com>
2018-04-03 13:56:05 -0400

    add a function removing duplicates in a linked list

--- Linked List/node.py
+++ Linked List/node.py
@@ -11,3 +11,27 @@ class Node():
         while node:
             print(node.val)
             node = node.next
+
+    def remove_duplicates(self):
+        ''' Remove duplicates from a linked list'''
+        ele = []
+        node = self
+        ele.append(node.val)
+        while node:
+            if node.next.val in ele:
+                node.next = node.next.next
+            else:
+                ele.append(node.next.val)
+            node = node.next
+                
+            
+if __name__ == "__main__":
+    node1 = Node(1)
+    node2 = Node(2)
+    node3 = Node(3)
+    node4 = Node(1)
+    node1.next = node2
+    node2.next = node3
+    node3.next = node4
+    node1.remove_duplicates()
+    node1.traverse()
